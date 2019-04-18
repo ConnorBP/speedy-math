095f7867449e46f8f7dd38b0a9b2737c9ad3f7de
Connor Postma <connor.postma@gmail.com>
2019-04-18 12:15:45 -0400

    divided travis build into test and build stages
    cleaned up the formatting and split up the travis buildscript into multiple manual build stages for testing and formatting and then build

--- speedy-math/src/main.rs
+++ speedy-math/src/main.rs
@@ -1,5 +1,3 @@
-
-
 /* use std::time::{Instant};
 
 macro_rules! benchmark {
@@ -25,14 +23,14 @@ macro_rules! namedbenchmark {
             println!("{} time: {:?} | {}", stringify!($($name)?), duration, stringify!(args));
         };
 } */
-#[macro_use]
+//#[macro_use]
 extern crate benchme;
-use std::time::{Instant};
+use benchme::{benchmark, benchmarknamed};
+use std::time::Instant;
 
 fn main() {
-
     benchmark! {
-        println!("omaewamou SHINDEIRUUUUUU! {}", 999999999);
+        println!("omaewamou SHINDEIRUUUUUU! {}", 999_999_999);
         println!("NANI!?!?!?!?");
     }
 
@@ -41,32 +39,28 @@ fn main() {
         println!("OwO notices 🚀");
         println!("the yeet was yote.");
     }
-
 }
 
-fn fast_between(value : i16, lower : i16, upper : i16) -> bool {
-// use a < for an inclusive lower bound and exclusive upper bound
-// use <= for an inclusive lower bound and inclusive upper bound
-// alternatively, if the upper bound is inclusive and you can pre-calculate
-//  upper-lower, simply add + 1 to upper-lower and use the < operator.
-  //  if ((unsigned)(value-lower) <= (upper-lower))
-  //      in_range(value);
+fn fast_between(value: i16, lower: i16, upper: i16) -> bool {
+    // use a < for an inclusive lower bound and exclusive upper bound
+    // use <= for an inclusive lower bound and inclusive upper bound
+    // alternatively, if the upper bound is inclusive and you can pre-calculate
+    //  upper-lower, simply add + 1 to upper-lower and use the < operator.
+    //  if ((unsigned)(value-lower) <= (upper-lower))
+    //      in_range(value);
 
     //let lu = (lower-upper) as u16;
     //let ul = upper-lower;
     //let vnorm = value-upper;
     //if vnorm < ul && < lu
-    ((value-lower) as u16) as i16 <= (upper - lower)
+    ((value - lower) as u16) as i16 <= (upper - lower)
 }
 
-fn slow_between(value : i16, lower : i16, upper : i16) -> bool {
+fn slow_between(value: i16, lower: i16, upper: i16) -> bool {
     value <= upper && value >= lower
 }
 
-fn sin_lut() {
-
-}
-
+fn sin_lut() {}
 
 #[cfg(test)]
 mod tests {
@@ -90,7 +84,11 @@ mod tests {
             let fast = fast_between(x, -4, 4);
             let slow = slow_between(x, -4, 4);
 
-            assert_eq!(fast, slow, "fast_between failed at x = `{}` fast: `{}` slow: `{}`", x, fast, slow);
+            assert_eq!(
+                fast, slow,
+                "fast_between failed at x = `{}` fast: `{}` slow: `{}`",
+                x, fast, slow
+            );
         }
     }
 }
