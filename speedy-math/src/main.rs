1722fc49d5ea7bf86619208b5c4bc67f9daff39c
Connor Postma <connor.postma@gmail.com>
2019-04-17 21:19:42 -0400

    initial commit
    created some macros for testing the timing of various functions. I intend to add various math calculations to this and compare their timings.

---
+++ speedy-math/src/main.rs
@@ -0,0 +1,86 @@
+/*
+#![feature(proc_macro_hygiene)]
+
+//put this in Cargo.toml for the procedural macro
+[dependencies]
+benchmark_macros = { version = "*", path = "../benchmark" }
+*/
+
+use std::time::{Duration, Instant};
+/* 
+use proc_macro::{Literal, Span, Ident, TokenStream, TokenTree};
+use proc_macro::quote; */
+
+/* macro_rules! benchmark1 {
+    ($t:expr) => {{
+        let start = Instant::now();
+        $t
+        let duration = start.elapsed();
+        println!("Time elapsed in expensive_function() is: {:?}", duration);
+    }}
+} */
+
+//use benchmark_macros::benchmark2;
+
+macro_rules! benchmark1 {
+    ($($lines:stmt;)*) => {
+        let start = Instant::now();
+        $($lines;)*
+        let duration = start.elapsed();
+        println!("Benchmark time: {:?}", duration);
+    };
+}
+
+/* macro_rules! benchmark3 {
+    (
+        $(#[$outer:meta])*
+        $($lines:stmt;)*
+    ) => {
+            let start = Instant::now();
+            $($lines;)*
+            let duration = start.elapsed();
+            println!("{} time: {:?}", #[meta], duration);
+        };
+} */
+
+fn main() {
+    
+/*  benchmark2! {
+        println!("Very challenging workload, I am.");
+        println!("Is it in between? {}.", if fast_between(0, -1, 1) {"Yes"} else {"No"});
+    } */
+
+    benchmark1! {
+        println!("omaewamou SHINDEIRUUUUUU! {}", 999999999);
+        println!("NANI!?!?!?!?");
+    }
+
+  /*   benchmark3! {
+        println!("OwO notices 🚀");
+        println!("the yeet was yote.");
+    } */
+
+    /* benchmark1! {{
+        //run codein here
+        println!("I am hard to run! :D");
+        println!("yeet");
+    }} */
+}
+
+
+
+
+fn fast_between(value : u16, lower : u16, upper : u16) -> bool {
+// use a < for an inclusive lower bound and exclusive upper bound
+// use <= for an inclusive lower bound and inclusive upper bound
+// alternatively, if the upper bound is inclusive and you can pre-calculate
+//  upper-lower, simply add + 1 to upper-lower and use the < operator.
+  //  if ((unsigned)(value-lower) <= (upper-lower))
+  //      in_range(value);
+
+    //let lu = (lower-upper) as u16;
+    //let ul = upper-lower;
+    //let vnorm = value-upper;
+    //if vnorm < ul && < lu
+    (value-lower) as u16 <= (upper - lower)
+}
